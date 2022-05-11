bf9a092d1844dbb354b1469c5308f8ed8e7bebbe
Luiz Moura <moura.oliveira.luiz@gmail.com>
2022-05-11 00:07:56 -0400

    Add Tailwindcss

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,7 @@
 const Encore = require('@symfony/webpack-encore');
+const PurgeCssPlugin = require('purgecss-webpack-plugin');
+const glob = require('glob-all');
+const path = require('path');
 
 // Manually configure the runtime environment if not already configured yet by the "encore" command.
 // It's useful when you use tools that rely on webpack.config.js file.
@@ -70,6 +73,12 @@ Encore
 
     // uncomment if you're having problems with a jQuery plugin
     //.autoProvidejQuery()
+
+    .enablePostCssLoader((options) => {
+        options.postcssOptions = {
+           config: './postcss.config.js'
+        };
+    })
 ;
 
 module.exports = Encore.getWebpackConfig();
