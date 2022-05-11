10d60f4e218ef577dbde53fd2ad67d00a183c2b1
Luiz Moura <moura.oliveira.luiz@gmail.com>
2022-05-11 11:37:39 -0400

    Add TwigComponent

--- config/bundles.php
+++ config/bundles.php
@@ -6,4 +6,5 @@ return [
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    Symfony\UX\TwigComponent\TwigComponentBundle::class => ['all' => true],
 ];
