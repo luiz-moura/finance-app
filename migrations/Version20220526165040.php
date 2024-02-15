561590df3d5c0c3335b2edc59ea993e21a1730d7
Luiz Moura <moura.oliveira.luiz@gmail.com>
2024-02-15 19:40:40 -0400

    fix: change id type to serial

--- migrations/Version20220526165040.php
+++ migrations/Version20220526165040.php
@@ -20,7 +20,7 @@ final class Version20220526165040 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE budget (id INT NOT NULL, category_id INT NOT NULL, name VARCHAR(255) NOT NULL, value NUMERIC(8, 2) NOT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE TABLE budget (id SERIAL NOT NULL, category_id INT NOT NULL, name VARCHAR(255) NOT NULL, value NUMERIC(8, 2) NOT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE INDEX IDX_73F2F77B12469DE2 ON budget (category_id)');
         $this->addSql('ALTER TABLE budget ADD CONSTRAINT FK_73F2F77B12469DE2 FOREIGN KEY (category_id) REFERENCES category (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
     }
