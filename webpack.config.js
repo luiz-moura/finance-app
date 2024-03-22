0916a6ca03f5c16d9637df2a99db664f9db76f93
Luiz Moura <moura.oliveira.luiz@gmail.com>
2024-03-21 20:19:10 -0400

    fix: change webpack plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,4 @@
 const Encore = require('@symfony/webpack-encore');
-const PurgeCssPlugin = require('purgecss-webpack-plugin');
-const glob = require('glob-all');
-const path = require('path');
 
 // Manually configure the runtime environment if not already configured yet by the "encore" command.
 // It's useful when you use tools that rely on webpack.config.js file.
@@ -49,7 +46,7 @@ Encore
     .enableVersioning(Encore.isProduction())
 
     .configureBabel((config) => {
-        config.plugins.push('@babel/plugin-proposal-class-properties');
+        config.plugins.push('@babel/plugin-transform-class-properties');
     })
 
     // enables @babel/preset-env polyfills
