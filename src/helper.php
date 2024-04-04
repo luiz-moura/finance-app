80f5e23fbffd306c0144ea204c40ff48ac9cc6b4
Luiz Moura <moura.oliveira.luiz@gmail.com>
2024-04-04 19:50:22 -0400

    feat: remove unused function

--- src/helper.php
+++ src/helper.php
@@ -1,17 +1,1 @@
 <?php
-
-function tofloat($num) {
-    $dotPos = strrpos($num, '.');
-    $commaPos = strrpos($num, ',');
-    $sep = (($dotPos > $commaPos) && $dotPos) ? $dotPos :
-        ((($commaPos > $dotPos) && $commaPos) ? $commaPos : false);
-
-    if (!$sep) {
-        return floatval(preg_replace("/[^0-9]/", "", $num));
-    }
-
-    return floatval(
-        preg_replace("/[^0-9]/", "", substr($num, 0, $sep)) . '.' .
-        preg_replace("/[^0-9]/", "", substr($num, $sep+1, strlen($num)))
-    );
-}
